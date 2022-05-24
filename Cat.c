f1d670877cfc608c1eb4bd8c5355cba1194f4f8b
JulienPeyrache <85626373+JulienPeyrache@users.noreply.github.com>
2022-05-24 11:39:10 +0200

    librairie stdlib au cas où

--- Cat.c
+++ Cat.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int cat(char *file_title){
     FILE *fd = fopen(file_title, "r");
--- Tee.c
+++ Tee.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 
 int tee(char *file_title){
     //Lire ce qu'il y a dans stdin, le réécrire dans stdout et l'écrire dans le fichier.
-    
+
     FILE *fd = fopen(file_title, "w");
     fputs(stdout, fd);
     fclose(fd);
