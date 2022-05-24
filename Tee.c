47eb75c81716f2d5dd8feb7c321b97d01ddac6b4
JulienPeyrache <85626373+JulienPeyrache@users.noreply.github.com>
2022-05-24 11:14:46 +0200

    ajout global

--- Tee.c
+++ Tee.c
@@ -2,6 +2,7 @@
 
 
 int tee(char *file_title){
+    //Lire ce qu'il y a dans stdin, le réécrire dans stdout et l'écrire dans le fichier.
     
     FILE *fd = fopen(file_title, "w");
     fputs(stdout, fd);
