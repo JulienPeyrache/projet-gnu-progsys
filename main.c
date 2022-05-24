5441c21b3ef5ad3367c7535c630327dc70aec9f9
JulienPeyrache <85626373+JulienPeyrache@users.noreply.github.com>
2022-05-24 10:53:13 +0200

    librairies pour le main

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+#include "Echo.h"
+#include "Cat.h"
+#include "Tee.h"
+#include "Rm.h"
+#include "Mkdir.h"
+#include "Ls.h"
+#include "Mv.h"
+#include "Grep.h"
+#include "Kill.h"
