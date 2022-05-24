fef6c04bc9472801af43fca483c8e84c5c6b3b08
JulienPeyrache <85626373+JulienPeyrache@users.noreply.github.com>
2022-05-24 10:44:09 +0200

    Cat C  v0

---
+++ Cat.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int cat(char *file_title){
+    FILE *fd = fopen(file_title, "r");
+    char c;
+    while((c = fgetc(fd)) != EOF){
+        putchar(c);
+    }
+    fclose(fd);
+    return 0;
+}
