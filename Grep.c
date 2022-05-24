651edf03d9e95c97d369d823116f0874fc85cfae
JulienPeyrache <85626373+JulienPeyrache@users.noreply.github.com>
2022-05-24 13:16:53 +0200

    Grep C v0

--- Grep.c
+++ Grep.c
@@ -1,2 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+int grep (char *filtre){
+    //Lire ce qu'il y a dans stdin, filtrer les champs qui nous concernent avec le filtre et les réécrire dans stdout.
+    char chaine[100];
+    
+    while (fgets(chaine, 100, stdin) != NULL){
+        if (strstr(chaine, filtre) != NULL){
+            printf("%s", chaine);
+        }
+    }
+    return 0;
+}
