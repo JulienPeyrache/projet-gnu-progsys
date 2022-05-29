df28dd6f00d0b80b08d8b465f467c6358db39071
JulienPeyrache <85626373+JulienPeyrache@users.noreply.github.com>
2022-05-30 01:14:15 +0200

    plus que core dump

--- main.c
+++ main.c
@@ -22,18 +22,44 @@ int echo(char* blabla){
     return 0;
 }
 
-int grep (char *filtre){
+int grep (char *filtre, char option){
     //Lire ce qu'il y a dans stdin, filtrer les champs qui nous concernent avec le filtre et les réécrire dans stdout.
     char chaine[100];
     FILE* stdin_custom = fopen("/dev/stdin", "r");
     FILE* stdout_custom = fopen("/dev/stdout", "w");
-
-
-    while (fgets(chaine, 100, stdin_custom) != NULL){
-        if (strstr(chaine, filtre) != NULL){
-            printf("%s", chaine);
+    while(fgets(chaine, 100, stdin_custom) != NULL){
+        if ((option=='v')&&(strstr(chaine, filtre) == NULL)){
+            fprintf(stdout_custom, "%s", chaine);
+            }
+        else if ((option =='a')&&(strstr(chaine, filtre) != NULL)){
+            fprintf(stdout_custom, "%s", chaine);
         }
-    }
+        else if (option =='i'){
+            void parcours(int i, char* chainecar){
+                if (i<strlen(chainecar)){
+                    char* chainecar_bis = chainecar;
+                    if ((chainecar[i]<90)&&(chainecar[i]>64)){
+                        chainecar_bis[i] = chainecar_bis[i]+32;
+                    }
+                    else if ((chainecar[i]<122)&&(chainecar[i]>96)){
+                        chainecar_bis[i] = chainecar_bis[i]-32;
+                    }
+                    if (strstr(chainecar_bis, filtre) != NULL){
+                        fprintf(stdout_custom, "%s", chainecar_bis);
+                    }
+                    parcours(i+1, chainecar_bis);
+                    parcours(i+1, chainecar);
+                }
+            }
+            if (strstr(chaine, filtre) != NULL){
+                fprintf(stdout_custom, "%s", chaine);
+            }
+            parcours(0, chaine);
+            }
+        }
+    fclose(stdin_custom);
+    stdout = stdin_custom;
+    fclose(stdout_custom);
     return 0;
 }
 
@@ -101,26 +127,26 @@ int rm (const char *pathname){
     return 0;
 }
 
-int tee(char *filename){
-    //read stdin, put it in a file and in stdout
+int tee(char* filename){
     FILE *fp = fopen(filename, "w");
-    FILE *stdin_bis = fopen("/dev/stdin", "r");
     FILE *stdout_bis = fopen("/dev/stdout", "w");
-    char *line = NULL;
-    size_t len = 0;
+    char *line = malloc(sizeof(char) * 100);
+    size_t len = 100;
     while(getline(&line, &len, stdin) != -1){
-        fprintf(fp, "%s", line);
+        fwrite(line, 1, 100, fp);
         fprintf(stdout_bis, "%s", line);
     }
+    stdout = stdout_bis;
+    fclose(fp);
     return 0;
 }
 
 int main()
 {
-    printf("%s", "Vous pouvez démarrer une ligne de commande:");
+    printf("%s", "Vous pouvez démarrer une ligne de commande. Tapez quit pour arrêter.");
     char* commande = "begin";
     while(strcmp(commande,"quit")!=0){
-        printf("%s", ">");
+        printf("%s", "\n >");
         scanf("%s", commande);
         arguments args = decomp(commande);
         if(strcmp(args.arg[0], "ls") == 0){
@@ -192,15 +218,26 @@ int main()
                 echo(args.arg[1]);
             }
         } else if(strcmp(args.arg[0], "grep") == 0){
-            if (args.nbarg != 2)
+            if (args.nbarg == 2)
             {
+                grep(args.arg[1], 'a');
+            }
+            else if (args.nbarg == 3)
+            {
+                if (strcmp(args.arg[1], "-i")==0){
+                    grep(args.arg[2],'i');}
+                else if (strcmp(args.arg[1], "-v")==0){
+                    grep(args.arg[2],'v');
+                }
+                else{
+                    printf("%s", "Erreur: arguments incorrects");
+                }
+            }
+            else {
                 printf("%s", "Erreur: nombre d'arguments incorrect");
             }
-            else
-            {
-                grep(args.arg[1]);
-            }
-        } else if(strcmp(args.arg[0], "kill") == 0){    //kill(pid, sig)
+        } else if(strcmp(args.arg[0], "kill") == 0){  
+
             if ((args.nbarg == 4)&&(strcmp(args.arg[1], "-s") == 0)){
                 kill_maison(atoi(args.arg[3]), atoi(args.arg[2]));
             } else if (args.nbarg == 3) {
